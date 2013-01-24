b5a60b4c4cc2a5c62c479a91497bacbeedacff06
Artem Sapegin <artem@sapegin.ru>
2013-01-24 16:47:51 +0400

    CLI.

---
+++ bin/cli.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+var argv = require('optimist')
+	.usage('Usage: $0 [--rules full] [--lang en] [--text] filename')
+	.boolean('text')
+	.default('rules', 'full')
+	.default('lang', 'en')
+	.demand(1)
+	.describe('rules', [
+		'Ruleset:',
+		'    - lite - Simple typographer (quotes, em-dash, etc.) for user generated content (e.g. comments);',
+		'    - rich - Enhancing typography: non-breaking spaces, abbreviations;',
+		'    - full - --light + --rich;',
+		'    - title - Typography for big text: the same as --rich + ampersands and hanging punctuation.'
+		].join('\n'))
+	.describe('lang', 'Text language.')
+	.argv;
+
+
+var richtypo = require('../richtypo.js');
+var fs = require('fs');
+var path = require('path');
+
+var lang = argv.lang;
+var file = argv._[0];
+
+if (!fs.existsSync(file)) {
+	console.log('File "' + file + '" not found.');
+	process.exit(1);
+}
+
+// Read UTF-8 file and remove BOM
+var text = fs.readFileSync(file, 'utf8').replace(/^\uFEFF/, '');
+
+text = richtypo[argv.rules](text, lang);
+
+if (argv.text) {
+	text = richtypo.textify(text, lang);
+}
+
+console.log(text);
