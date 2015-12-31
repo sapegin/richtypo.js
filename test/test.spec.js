726b972bf22003740be3e23d819224d18daf4690
Artem Sapegin <artem@sapegin.ru>
2015-12-31 12:32:23 +0100

    Improve tests readability.

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,16 +1,18 @@
 var expect = require('chai').expect;
 var rt = require('../richtypo');
 
-function _symb(s) {
-	return s.replace(/_/g, String.fromCharCode(160));
+function compare(actual, expected) {
+	expect(actual.replace(/\xA0/g, '_')).to.eql(expected);
 }
 
 describe('RichTypo', function() {
 
 	it('has API functions', function() {
 		rt.lang('ru');
-		expect(rt.lite('Такса — животное.'))
-			.to.eql(_symb('Такса_— животное.'));
+		compare(
+			rt.lite('Такса — животное.'),
+			'Такса_— животное.'
+		);
 	});
 
 	it('has API functions', function() {
@@ -22,169 +24,254 @@ describe('RichTypo', function() {
 	it('safe tags ru', function() {
 		rt.lang('ru');
 
-		expect(rt.rich('<code> -->> </code>'))
-			.to.eql('<code> -->> </code>');
+		compare(
+			rt.rich('<code> -->> </code>'),
+			'<code> -->> </code>'
+		);
 
-		expect(rt.rich('<code> а — б </code>'))
-			.to.eql('<code> а — б </code>');
+		compare(
+			rt.rich('<code> а — б </code>'),
+			'<code> а — б </code>'
+		);
+		
+		compare(
+			rt.rich('А текст внутри тегов <img src="hamster.jpg" alt="а — б"> не надо типографить.'),
+			'А_текст внутри тегов <img src="hamster.jpg" alt="а — б"> не_надо типографить.'
+		);
 
-		expect(rt.rich('А текст внутри тегов <img src="hamster.jpg" alt="а — б"> не надо типографить.'))
-			.to.eql(_symb('А_текст внутри тегов <img src="hamster.jpg" alt="а — б"> не_надо типографить.'));
-
-		expect(rt.rich('Теперь <b>всё — вместе</b>. А текст внутри тегов <img src="hamster.jpg" alt="а — б"> не надо типографить. И кода кусок:<pre><code>\nа — б\nа — б\nа — б\n</code></pre>.'))
-			.to.eql(_symb('Теперь <b>всё_— вместе</b>. А_текст внутри тегов <img src="hamster.jpg" alt="а — б"> не_надо типографить. И_кода кусок:<pre><code>\nа — б\nа — б\nа — б\n</code></pre>.'));	
+		compare(
+			rt.rich('Теперь <b>всё — вместе</b>. А текст внутри тегов <img src="hamster.jpg" alt="а — б"> не надо типографить. И кода кусок:<pre><code>\nа — б\nа — б\nа — б\n</code></pre>.'),
+			'Теперь <b>всё_— вместе</b>. А_текст внутри тегов <img src="hamster.jpg" alt="а — б"> не_надо типографить. И_кода кусок:<pre><code>\nа — б\nа — б\nа — б\n</code></pre>.'
+		);
 	});	
 
 	it('spaces lite ru', function() {
 		rt.lang('ru');
 
-		expect(rt.rich('Такса — животное.'))
-			.to.eql(_symb('Такса_— животное.'));
+		compare(
+			rt.rich('Такса — животное.'),
+			'Такса_— животное.'
+		);
 
-		expect(rt.rich('Там было много тлонов, тутликов, табачек и т. п. и т. п.'))
-			.to.eql(_symb('Там было много тлонов, тутликов, табачек и_т._п. и_т._п.'));		
+		compare(
+			rt.rich('Там было много тлонов, тутликов, табачек и т. п. и т. п.'),
+			'Там было много тлонов, тутликов, табачек и_т._п. и_т._п.'
+		);
 	});
 
 	it('spaces ru', function() {
 		rt.lang('ru');
 
-		expect(rt.rich('Прочитай § 13 журнала «Такса» № 27, там про тлонов, тутликов, табачек.'))
-			.to.eql(_symb('Прочитай §_13 журнала «Такса» №_27, там про тлонов, тутликов, табачек.'));
+		compare(
+			rt.rich('Прочитай § 13 журнала «Такса» № 27, там про тлонов, тутликов, табачек.'),
+			'Прочитай §_13 журнала «Такса» №_27, там про тлонов, тутликов, табачек.'
+		);
 
-		expect(rt.rich('Приснился однажды В. И. Ленин В. Путину.'))
-			.to.eql('Приснился однажды <nobr>В. И. Ленин</nobr> <nobr>В. Путину</nobr>.');
+		compare(
+			rt.rich('Приснился однажды В. И. Ленин В. Путину.'),
+			'Приснился однажды <nobr>В. И. Ленин</nobr> <nobr>В. Путину</nobr>.'
+		);
 
-		expect(rt.rich('Из-за чего-то всё-таки это как-то очень странно.'))
-			.to.eql('<nobr>Из-за</nobr> <nobr>чего-то</nobr> всё-таки это <nobr>как-то</nobr> очень странно.');
+		compare(
+			rt.rich('Из-за чего-то всё-таки это как-то очень странно.'),
+			'<nobr>Из-за</nobr> <nobr>чего-то</nobr> всё-таки это <nobr>как-то</nobr> очень странно.'
+		);
 
-		expect(rt.rich('За колбасой в магазин пошла такса из резины.'))
-			.to.eql(_symb('За_колбасой в_магазин пошла такса из_резины.'));
+		compare(
+			rt.rich('За колбасой в магазин пошла такса из резины.'),
+			'За_колбасой в_магазин пошла такса из_резины.'
+		);
 
-		expect(rt.rich('Это ж как бы колбаса!'))
-			.to.eql(_symb('Это_ж как_бы колбаса!'));
+		compare(
+			rt.rich('Это ж как бы колбаса!'),
+			'Это_ж как_бы колбаса!'
+		);
 
-		expect(rt.rich('Колбаса стоит 23 р. Такса стоит 1000000 $.'))
-			.to.eql(_symb('Колбаса стоит 23_р. Такса стоит 1000000_$.'));
+		compare(
+			rt.rich('Колбаса стоит 23 р. Такса стоит 1000000 $.'),
+			'Колбаса стоит 23_р. Такса стоит 1000000_$.'
+		);
 
-		expect(rt.rich('Это было в 1927 г.'))
-			.to.eql(_symb('Это было в_1927_г.'));
+		compare(
+			rt.rich('Это было в 1927 г.'),
+			'Это было в_1927_г.'
+		);
 
-		expect(rt.rich('Цири родилась 30 июля.'))
-			.to.eql(_symb('Цири родилась 30_июля.'));
+		compare(
+			rt.rich('Цири родилась 30 июля.'),
+			'Цири родилась 30_июля.'
+		);
+
+		//compare(
+		//  rt.rich('рынка книги, мы к этой цифре'),
+		//	'рынка книги, мы_к_этой цифре'
+		// );
 	});
 
 	it('lite ru', function() {
 		rt.lang('ru');
 
-		expect(rt.lite('Эх...'))
-			.to.eql('Эх…');
+		compare(
+			rt.lite('Эх...'),
+			'Эх…'
+		);
 
-		expect(rt.lite('Такса -- зверь'))
-			.to.eql(_symb('Такса_— зверь'));
+		compare(
+			rt.lite('Такса -- зверь'),
+			'Такса_— зверь'
+		);
 
-		expect(rt.lite('Такса - зверь'))
-			.to.eql(_symb('Такса_— зверь'));
+		compare(
+			rt.lite('Такса - зверь'),
+			'Такса_— зверь'
+		);
 
-		expect(rt.lite('<p>- Бадыдыщь!</p>'))
-			.to.eql(_symb('<p>— Бадыдыщь!</p>'));
+		compare(
+			rt.lite('<p>- Бадыдыщь!</p>'),
+			'<p>— Бадыдыщь!</p>'
+		);
 
-		expect(rt.lite('- Бадыдыщь!\n- Бадыдыщь!'))
-			.to.eql('&mdash; Бадыдыщь!\n&mdash; Бадыдыщь!');
+		compare(
+			rt.lite('- Бадыдыщь!\n- Бадыдыщь!'),
+			'&mdash; Бадыдыщь!\n&mdash; Бадыдыщь!'
+		);
 
-		expect(rt.lite('<p>- Бадыдыщь!</p>'))
-			.to.eql(_symb('<p>— Бадыдыщь!</p>'));
+		compare(
+			rt.lite('<p>- Бадыдыщь!</p>'),
+			'<p>— Бадыдыщь!</p>'
+		);
 	});
 
 	it('lite en', function() {
 		rt.lang('en');
 
-		expect(rt.lite('Yep...'))
-			.to.eql('Yep…');
+		compare(
+			rt.lite('Yep...'),
+			'Yep…'
+		);
 
-		expect(rt.lite('Dachshund--beast'))
-			.to.eql(_symb('<nobr>Dachshund—</nobr>beast'));
+		compare(
+			rt.lite('Dachshund--beast'),
+			'<nobr>Dachshund—</nobr>beast'
+		);
 	});
 
 	it('quotes ru', function() {
 		rt.lang('ru');
 
-		expect(rt.lite('Тут просто "текст в кавычках".'))
-			.to.eql('Тут просто «текст в кавычках».');
+		compare(
+			rt.lite('Тут просто "текст в кавычках".'),
+			'Тут просто «текст в кавычках».'
+		);
 
-		expect(rt.lite('"Текст в кавычках "в кавычках"".'))
-			.to.eql('«Текст в кавычках «в кавычках»».');
+		compare(
+			rt.lite('"Текст в кавычках "в кавычках"".'),
+			'«Текст в кавычках «в кавычках»».'
+		);
 
-		expect(rt.lite('А тут "текст "в кавычках" в кавычках".'))
-			.to.eql('А тут «текст «в кавычках» в кавычках».');
+		compare(
+			rt.lite('А тут "текст "в кавычках" в кавычках".'),
+			'А тут «текст «в кавычках» в кавычках».'
+		);
 
-		expect(rt.lite('И "клинический "случай".'))
-			.to.eql('И «клинический «случай».');
+		compare(
+			rt.lite('И "клинический "случай".'),
+			'И «клинический «случай».'
+		);
 	});
 
 	it('quotes en', function() {
 		rt.lang('en');
 
-		expect(rt.lite('There is a "text in quotes."'))
-			.to.eql('There is a “text in quotes.”');
+		compare(
+			rt.lite('There is a "text in quotes."'),
+			'There is a “text in quotes.”'
+		);
 
-		expect(rt.lite('There is a "text "in quotes" in quotes."'))
-			.to.eql('There is a “text “in quotes” in quotes.”');
+		compare(
+			rt.lite('There is a "text "in quotes" in quotes."'),
+			'There is a “text “in quotes” in quotes.”'
+		);
 	});
 
 	it('amps en', function() {
 		rt.lang('en');
 
-		expect(rt.title('Dessi &amp; Tsiri'))
-			.to.eql('Dessi <span class="amp">&amp;</span> Tsiri');
+		compare(
+			rt.title('Dessi &amp; Tsiri'),
+			'Dessi <span class="amp">&amp;</span> Tsiri'
+		);
 	});
 
 	it('abbrs ru', function() {
 		rt.lang('ru');
 
-		expect(rt.title('На ХХ таксовке ТАКСА было 37 такс'))
-			.to.eql(_symb('На_ХХ таксовке <abbr>ТАКСА</abbr> было 37 такс'));
+		compare(
+			rt.title('На ХХ таксовке ТАКСА было 37 такс'),
+			'На_ХХ таксовке <abbr>ТАКСА</abbr> было 37 такс'
+		);
 	});
 
 	it('abbrs en', function() {
 		rt.lang('en');
 
-		expect(rt.title('On XX dachshund party DOXIE was 37 wieners'))
-			.to.eql(_symb('On_XX dachshund party <abbr>DOXIE</abbr> was 37 wieners'));
+		compare(
+			rt.title('On XX dachshund party DOXIE was 37 wieners'),
+			'On_XX dachshund party <abbr>DOXIE</abbr> was 37 wieners'
+		);
 	});
 
 	it('hanging', function() {
-		expect(rt.title('The “quoted text.”'))
-			.to.eql(_symb('The<span class="sldquo"> </span> <span class="hldquo">“</span>quoted text.”'));
+		compare(
+			rt.title('The “quoted text.”'),
+			'The<span class="sldquo"> </span> <span class="hldquo">“</span>quoted text.”'
+		);
 
-		expect(rt.title('“Quoted text” two.'))
-			.to.eql(_symb('<span class="hldquo">“</span>Quoted text” two.'));
+		compare(
+			rt.title('“Quoted text” two.'),
+			'<span class="hldquo">“</span>Quoted text” two.'
+		);
 
-		expect(rt.title('<p>“Quoted text” three.</p>'))
-			.to.eql(_symb('<p><span class="hldquo">“</span>Quoted text” three.</p>'));
+		compare(
+			rt.title('<p>“Quoted text” three.</p>'),
+			'<p><span class="hldquo">“</span>Quoted text” three.</p>'
+		);
 
-		expect(rt.title('alert("Hello world!")'))
-			.to.eql(_symb('alert("Hello world!")'));
+		compare(
+			rt.title('alert("Hello world!")'),
+			'alert("Hello world!")'
+		);
 	});	
 
 	it('textify', function() {
-		expect(rt.textify(rt.title('The “quoted text.”')))
-			.to.eql(_symb('The “quoted text.”'));
+		expect(
+			rt.textify(rt.title('The “quoted text.”')),
+			'The “quoted text.”'
+		);
 
-		expect(rt.textify(rt.lite('- Бадыдыщь!\n- Бадыдыщь!', 'ru')))
-			.to.eql('— Бадыдыщь!\n— Бадыдыщь!');
+		expect(
+			rt.textify(rt.lite('- Бадыдыщь!\n- Бадыдыщь!', 'ru')),
+			'— Бадыдыщь!\n— Бадыдыщь!'
+		);
 	});
 
 	it('leaves commented out tags alone', function() {
-		expect(rt.lite('<!-- <script>alert("wheee");</script><style>* { color: red; }</style><pre>...</pre> -->'))
-			.to.eql('<!-- <script>alert("wheee");</script><style>* { color: red; }</style><pre>...</pre> -->');
+		compare(
+			rt.lite('<!-- <script>alert("wheee");</script><style>* { color: red; }</style><pre>...</pre> -->'),
+			'<!-- <script>alert("wheee");</script><style>* { color: red; }</style><pre>...</pre> -->'
+		);
 	});
 
 	it('plays nice with ie conditional comments', function() {
-		expect(rt.title('<!--[if lte IE 6]><script>alert("wheee");</script><style>* { color: red; }</style><pre>...</pre><![endif]-->'))
-			.to.eql('<!--[if lte IE 6]><script>alert("wheee");</script><style>* { color: red; }</style><pre>...</pre><![endif]-->');
+		compare(
+			rt.title('<!--[if lte IE 6]><script>alert("wheee");</script><style>* { color: red; }</style><pre>...</pre><![endif]-->'),
+			'<!--[if lte IE 6]><script>alert("wheee");</script><style>* { color: red; }</style><pre>...</pre><![endif]-->'
+		);
 
-		expect(rt.title('<!--[if lte IE 6]>The “quoted text.”<![endif]-->'))
-			.to.eql('<!--[if lte IE 6]>The<span class="sldquo"> </span> <span class="hldquo">“</span>quoted text.”<![endif]-->');
+		compare(
+			rt.title('<!--[if lte IE 6]>The “quoted text.”<![endif]-->'),
+			'<!--[if lte IE 6]>The<span class="sldquo"> </span> <span class="hldquo">“</span>quoted text.”<![endif]-->'
+		);
 	});
 
 });
