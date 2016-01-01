77dd5401d45e2f5099eb31c15db17bf11aa12f2d
Artem Sapegin <artem@sapegin.ru>
2016-01-01 18:01:08 +0100

    Updates

---
+++ src/index.js
@@ -0,0 +1,39 @@
+var path = require('path');
+var sweet2 = require('sweet2');
+var richtypo = require('../../richtypo');
+
+richtypo.lang('en');
+
+var config = {
+	base: {
+		lang: 'en'
+	}
+};
+
+var renderMarkdown = sweet2.createMarkdownRenderer();
+var renderTemplate = sweet2.createTemplateRenderer({
+	root:__dirname
+});
+
+var documents = sweet2.loadSourceFiles(__dirname, ['md'], {
+	renderers: {
+		md: renderMarkdown
+	}
+});
+
+documents = documents.map(function(doc) {
+	// Run Rich Typo
+	doc.pageTitle = richtypo.title(doc.title);  // Titles
+	doc.content = richtypo.rich(doc.content);  // Page content
+	doc.content = richtypo.richtypo(doc.content, ['hanging']);  // Hanging punctuation
+
+	// Make non-breaking spaces visible
+	doc.pageTitle = doc.pageTitle.replace(/ /g, '&nbsp;');
+	doc.content = doc.content.replace(/ /g, '&nbsp;');
+
+	return doc;
+});
+
+var pages = sweet2.generatePages(documents, config, sweet2.helpers, {ect: renderTemplate});
+
+sweet2.savePages(pages, path.resolve(__dirname, '..'));
