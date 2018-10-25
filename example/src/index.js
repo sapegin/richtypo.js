1101e7d6bdf0276776969e599595ce7cc0fd92e0
David Bismut <david.bismut@gmail.com>
2018-10-25 22:32:20 +0200

    Linting fix

--- example/src/index.js
+++ example/src/index.js
@@ -1,12 +1,12 @@
 import path from 'path';
 import {
-  start,
-  loadSourceFiles,
-  generatePages,
-  savePages,
-  createMarkdownRenderer,
-  createTemplateRenderer,
-  helpers
+	start,
+	loadSourceFiles,
+	generatePages,
+	savePages,
+	createMarkdownRenderer,
+	createTemplateRenderer,
+	helpers,
 } from 'fledermaus';
 
 import richtypo from '../../src/richtypo';
@@ -14,20 +14,20 @@ import ukRuleset from '../../packages/uk/src/uk';
 import frenchRuleset from '../../packages/french/src/french';
 
 function decorateRules(ruleset) {
-  const transformedRules = {};
-  Object.entries(ruleset).forEach(([name, rules]) => {
-    transformedRules[name] = rules.map(([search, replace]) => [
-      search,
-      `<span class="rule ${name}" title="${name}">${replace}</span>`
-    ]);
-  });
+	const transformedRules = {};
+	Object.entries(ruleset).forEach(([name, rules]) => {
+		transformedRules[name] = rules.map(([search, replace]) => [
+			search,
+			`<span class="rule ${name}" title="${name}">${replace}</span>`,
+		]);
+	});
 
-  return transformedRules;
+	return transformedRules;
 }
 
 const rt = {
-  uk: richtypo(decorateRules(ukRuleset)),
-  fr: richtypo(decorateRules(frenchRuleset))
+	uk: richtypo(decorateRules(ukRuleset)),
+	fr: richtypo(decorateRules(frenchRuleset)),
 };
 
 const config = { base: { lang: 'en' } };
@@ -38,18 +38,18 @@ const renderMarkdown = createMarkdownRenderer();
 const renderTemplate = createTemplateRenderer({ root: __dirname });
 
 let documents = loadSourceFiles(__dirname, ['md'], {
-  renderers: { md: renderMarkdown }
+	renderers: { md: renderMarkdown },
 });
 
 documents = documents.map(doc => ({
-  ...doc,
-  pageTitle: rt[doc.language].all(doc.title).html(),
-  richtypo: rt[doc.language].all(doc.content).html(),
-  content: doc.content // Page content
+	...doc,
+	pageTitle: rt[doc.language].all(doc.title).html(),
+	richtypo: rt[doc.language].all(doc.content).html(),
+	content: doc.content, // Page content
 }));
 
 const pages = generatePages(documents, config.base, helpers, {
-  jsx: renderTemplate
+	jsx: renderTemplate,
 });
 
 savePages(pages, path.resolve(__dirname, '../build'));
