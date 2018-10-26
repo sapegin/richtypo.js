c2c0cf41a13a855a805fec640d09adbc94286c5a
David Bismut <david.bismut@gmail.com>
2018-10-26 22:33:31 +0200

    Update src/__tests__/api.test.js
    Added tests for general API

---
+++ src/__tests__/api.test.js
@@ -0,0 +1,33 @@
+import rt from '../richtypo';
+
+const mockRule = [[/\d/gim, '_']];
+const mockFunction = [text => text.toUpperCase()];
+
+describe('api test', () => {
+	it('should run the fake rule that changes numbers with underscores', () => {
+		expect(rt(mockRule, `changes number 100 with underscores`)).toEqual(
+			`changes number ___ with underscores`
+		);
+	});
+	it('should run the curried fake rule that changes numbers with underscores', () => {
+		const curriedMockRule = rt(mockRule);
+		expect(curriedMockRule(`changes number 100 with underscores`)).toEqual(
+			`changes number ___ with underscores`
+		);
+	});
+	it('should run the mock function that changes the text in uppercase', () => {
+		expect(rt(mockFunction, `changes this sentence in uppercase`)).toEqual(
+			`CHANGES THIS SENTENCE IN UPPERCASE`
+		);
+	});
+	it('should run both the mock rule and mock function', () => {
+		expect(
+			rt([mockRule, mockFunction], `changes number 100 with underscores`)
+		).toEqual(`CHANGES NUMBER ___ WITH UNDERSCORES`);
+	});
+	it('should run both the mock function and mock rule', () => {
+		expect(
+			rt([mockFunction, mockRule], `changes number 100 with underscores`)
+		).toEqual(`CHANGES NUMBER ___ WITH UNDERSCORES`);
+	});
+});
