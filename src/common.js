7a81a17ba64e313836b609c38d628838d58da6e8
David Bismut <david.bismut@gmail.com>
2018-10-27 13:54:58 +0200

    Added temperature rule and spaces rule is composed differently

--- src/common.js
+++ src/common.js
@@ -5,7 +5,7 @@ export const defs = {
 	tag: '<[^<>]*>',
 	quotes: '["“”«»‘’]',
 	letters: '[a-zà-ž0-9]',
-	upperletters: '[A-ZÀ-Ž0-9]',
+	upperletters: '[A-ZÀ-Ž]',
 	nonletters: '[^a-zà-ž0-9]',
 	letterswithquotes: '[a-zà-ž0-9-“”‘’«»]',
 	semicolon: '(?<!&\\S*);',
@@ -20,77 +20,82 @@ export const defs = {
 };
 
 // Non-breaking space after short words
-const shortWordsBreak = ({ shortWord, quotes, space, nbsp, tag }) => ({
+export const shortWordBreak = ({ shortWord, quotes, space, nbsp, tag }) => ({
 	regex: `(?<=^|${space}|${quotes}|>)(${shortWord}(${tag})?)${space}`,
-	result: `$1${nbsp}`,
+	replace: `$1${nbsp}`,
 });
 
 // Orphans (non-breaking space before the last word)
 export const orphans = ({ space, nbsp }) => ({
 	regex: `${space}([\\S<]{1,10}(?:\n\n|$))`,
-	result: `${nbsp}$1`,
+	replace: `${nbsp}$1`,
 });
 
-export const numbersUnits = ({ space, nbsp }) => ({
+export const numberUnits = ({ space, nbsp }) => ({
 	regex: `(\\d+)${space}(\\w)`,
-	result: `$1${nbsp}$2`,
+	replace: `$1${nbsp}$2`,
 });
 
-export const spaces = [numbersUnits, shortWordsBreak, orphans];
+export const temperature = ({ space, nbsp }) => ({
+	regex: `(\\d)${space}?°`,
+	replace: `$1${nbsp}°`,
+});
+
+export const spaces = { numberUnits, temperature, shortWordBreak, orphans };
 
 export const emdash = [
 	({ space, nbsp }) => ({
 		regex: `(\\S)${space}?—`,
-		result: `$1${nbsp}—`,
+		replace: `$1${nbsp}—`,
 	}),
 	{
 		regex: `—(\\S)`,
-		result: `— $1`,
+		replace: `— $1`,
 	},
 	({ space, nbsp, letterswithquotes, tag, dash }) => ({
 		regex: `(${letterswithquotes}(${tag})?)${space}${dash}`,
-		result: `$1${nbsp}—`,
+		replace: `$1${nbsp}—`,
 	}),
 	({ space, nbsp, punctuation, openingQuote, dash }) => ({
 		regex: `(^|(?:(${punctuation}|${openingQuote}|")${space}?))${dash}${space}`,
-		result: `$1—${nbsp}`,
+		replace: `$1—${nbsp}`,
 	}),
 ];
 
 export const ellipsis = {
 	regex: `\\.{2,}`,
-	result: `…`,
+	replace: `…`,
 };
 
 export const amp = ({ space, nbsp }) => ({
 	regex: `${space}(&(?!\\S*;))${space}`,
-	result: `${nbsp}<span class="amp">&</span>${nbsp}`,
+	replace: `${nbsp}<span class="amp">&</span>${nbsp}`,
 });
 
 export const abbr = ({ upperletters }) => ({
 	regex: `(${upperletters}{3,})`,
 	flags: `gm`,
-	result: `<abbr>$1</abbr>`,
+	replace: `<abbr>$1</abbr>`,
 });
 
 export const numbers = [
 	({ ordinals }) => ({
 		regex: `(\\d+)(${ordinals})`,
-		result: `$1<sup>$2</sup>`,
+		replace: `$1<sup>$2</sup>`,
 	}),
 	({ decimalsSeparator, thousandsSeparator }) => ({
 		regex: `(?<!${decimalsSeparator}\\d*)\\d{1,3}(?=(\\d{3})+(?!\\d))`,
-		result: `$&${thousandsSeparator}`,
+		replace: `$&${thousandsSeparator}`,
 	}),
 ];
 
 export const quotes = [
 	({ tag, letters, dash, space, openingQuote }) => ({
 		regex: `"((${tag})?(${dash}${space})?${letters})`,
-		result: `${openingQuote}$1`,
+		replace: `${openingQuote}$1`,
 	}),
 	({ closingQuote }) => ({
 		regex: `"`,
-		result: `${closingQuote}`,
+		replace: `${closingQuote}`,
 	}),
 ];
