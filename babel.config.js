ce492668c6db48abed2acfe1034f1793da57feff
David Bismut <david.bismut@gmail.com>
2018-10-30 18:29:32 +0100

    Moving and renaming files

--- babel.config.js
+++ babel.config.js
@@ -1,38 +1,38 @@
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: { node: '6' }
-      }
-    ]
-  ],
-  env: {
-    local: {
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            targets: { node: 'current' }
-          }
-        ]
-      ],
-      plugins: [
-        [
-          'module-resolver',
-          {
-            alias: {
-              richtypo: './src/richtypo.js'
-            }
-          }
-        ],
-        [
-          'transform-react-jsx',
-          {
-            pragma: 'vdo'
-          }
-        ]
-      ]
-    }
-  }
+	presets: [
+		[
+			'@babel/preset-env',
+			{
+				targets: { node: '6' },
+			},
+		],
+	],
+	env: {
+		local: {
+			presets: [
+				[
+					'@babel/preset-env',
+					{
+						targets: { node: 'current' },
+					},
+				],
+			],
+			plugins: [
+				[
+					'module-resolver',
+					{
+						alias: {
+							richtypo: './packages/richtypo/src/richtypo.js',
+						},
+					},
+				],
+				[
+					'transform-react-jsx',
+					{
+						pragma: 'vdo',
+					},
+				],
+			],
+		},
+	},
 };
