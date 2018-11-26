33c566582b806d45a6609bc3f3ed0a84395e7a58
Artem Sapegin <asapegin@wayfair.com>
2018-11-26 09:45:53 +0100

    Fix: French — remove amps rule from recommended rules

--- packages/richtypo-rules-fr/src/fr.js
+++ packages/richtypo-rules-fr/src/fr.js
@@ -8,7 +8,6 @@ import {
 	numberSeparatorsFactory,
 	numberUnits,
 	degreeSigns,
-	amps,
 	dashesBasic,
 	ellipses,
 } from 'richtypo-rules-common';
@@ -67,6 +66,7 @@ export {
 } from 'richtypo-rules-common';
 
 // Not in recommended:
+// - amps
 // - numberOrdinals
 // - numberSeparators - breaks years, like "1920"
 
@@ -75,7 +75,6 @@ const recommended = [
 	shortWords,
 	orphans,
 	abbrs,
-	amps,
 	dashes,
 	ellipses,
 	numberUnits,
