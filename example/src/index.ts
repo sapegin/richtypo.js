14e7265b2dd9278b5483fbd898d59e73b9f0889b
Artem Sapegin <artem@sapegin.ru>
2024-06-28 07:36:52 +0200

    feat: Upgrade to Node.js 20 (#66)
    BREAKING CHANGE:
    
    Node.js 20 is the minimum supported version

--- example/src/index.ts
+++ example/src/index.ts
@@ -5,7 +5,36 @@ import richtypo from 'richtypo';
 import enRules, { amps as enAmps } from 'richtypo-rules-en';
 import frRules, { amps as frAmps } from 'richtypo-rules-fr';
 import ruRules from 'richtypo-rules-ru';
-import { template } from './template';
+
+function template({
+	lang,
+	title,
+	content,
+	contentTypo,
+}: {
+	lang: string;
+	title: string;
+	content: string;
+	contentTypo: string;
+}) {
+	return `<html lang="${lang}">
+			<head>
+				<meta charset="utf-8" />
+				<title>${title}</title>
+				<link
+					href="https://fonts.googleapis.com/css?family=Vollkorn:400,400i,700i&amp;subset=cyrillic,latin-ext"
+					rel="stylesheet"
+				/>
+				<link href="example.css" rel="stylesheet" />
+			</head>
+			<body>
+				<main role="main" class="content">
+					<div class="content__column">${content}</div>
+					<div class="content__column">${contentTypo}</div>
+				</main>
+			</body>
+		</html>`;
+}
 
 console.log('Building the example site...');
 
